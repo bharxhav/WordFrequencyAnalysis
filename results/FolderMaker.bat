5aac987464af2d856bad51cc5af0019b7ee39a42
Bhargav Kantheti <70275374+bharxhav@users.noreply.github.com>
2020-12-14 20:49:30 +0530

    Update Batch File

---
+++ results/FolderMaker.bat
@@ -0,0 +1,12 @@
+cd H:\Projects-DSP-Case_Study\WordFrequencyAnalysis\results
+
+mkdir 2001_ASpaceOdyssey
+mkdir BladeRunner
+mkdir Dune
+mkdir FightClub
+mkdir Interstellar_Mandarin
+mkdir LoremIpsumFiller
+mkdir MadMax
+mkdir Matrix
+mkdir Memento
+mkdir StarWars_EmpireStrikesBack
